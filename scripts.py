be5f82907f4e304380459e8eb2da54cca9805227
SirBubbls <lucassas@live.de>
2020-09-13 13:38:05 +0200

    condense pruning operations are used

--- scripts.py
+++ scripts.py
@@ -4,27 +4,6 @@ import matplotlib.pyplot as plt
 
 fig_font_size = 30
 
-def prune(model, sparsity):
-    new_weights = []
-    for layer in model.get_weights():
-        new_weights.append(weight_prune_layer(layer, sparsity))
-    model.set_weights(new_weights)
-    return model
-
-def weight_prune_layer(layer, sparsity):
-    """
-    Prunes the lowest p% of
-    args:
-      - layer: The target layer that should get new[0::2][layer]
-      - sparsity: The percentage of desired sparsity
-    """
-
-    original_shape = layer.shape
-    layer = layer.flatten()
-
-    s = np.argsort(np.abs(layer.flatten()))
-    layer[s > int(len(layer) * (1 - sparsity))] = 0
-    return layer.reshape(original_shape)
 
 def visualize_kernel(kernel, fig):
     """
@@ -32,33 +11,25 @@ def visualize_kernel(kernel, fig):
     returns: plot of weights
     """
     im = fig.imshow(np.abs(kernel),
-               interpolation='none',
-               cmap='inferno',
-            clim=0)
+                    interpolation='none',
+                    cmap='inferno',
+                    clim=0)
     plt.colorbar(im, ax=fig)
     return fig
 
-def unit_prune_layer(layer, sparsity):
-    shape = layer.shape
-    layer = np.array(layer)
-    neuron_weights = abs(layer).sum(0)
-
-    layer[:, np.argsort(neuron_weights) < int(len(neuron_weights) * sparsity)] = 0
-
-    return layer.reshape(shape)
 
 def quad_plot(new, old, output):
-    fig = plt.figure(constrained_layout=True, figsize=(24,12))
+    fig = plt.figure(constrained_layout=True, figsize=(24, 12))
     gs = plt.GridSpec(2, 2, figure=fig)
     ax2 = fig.add_subplot(gs[0, 0])
     ax3 = fig.add_subplot(gs[0, 1])
     ax4 = fig.add_subplot(gs[1, 0])
     ax5 = fig.add_subplot(gs[1, 1])
 
-    ax4.hist(old.flatten(), density=False, width=.1, bins='auto')
+    ax4.hist(np.abs(old.flatten()), density=False, width=.1, bins='auto')
     ax4.set_title("Original Weight Distribution", fontsize=fig_font_size)
 
-    ax5.hist(new.flatten(), density=False, width=.1, bins='auto')
+    ax5.hist(np.abs(new.flatten()), density=False, width=.1, bins='auto')
     ax5.set_title("Pruned Weight Distribution", fontsize=fig_font_size)
 
     visualize_kernel(old, ax2)
@@ -69,6 +40,7 @@ def quad_plot(new, old, output):
 
     plt.savefig(output)
     return output
+
 
 def calculate_layer_sparsity(layer):
     """
@@ -92,14 +64,3 @@ def calculate_model_sparsity(model):
                       weights=[np.product(layer.shape) for layer in model])
 
 
-def visualize_kernel(kernel, fig):
-    """
-    Displays kernel weight as a plot.
-    returns: plot of weights
-    """
-    im = fig.imshow(np.abs(kernel),
-               interpolation='none',
-               cmap='inferno')
-    # plt.colorbar()
-    # fig.clim(0)
-    return fig
